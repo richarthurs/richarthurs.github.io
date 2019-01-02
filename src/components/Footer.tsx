7b321cc9c54527f8ef545c4a6b064913da00761c
richarthurs <arthursrich@gmail.com>
2019-01-02 15:24:41 -0800

    Working site, tweaked footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -74,22 +74,13 @@ const Footer: React.FunctionComponent = () => {
         </section>
         <SiteFooterNav>
           <Link to="/">Latest Posts</Link>
-          {config.facebook && (
-            <a href={config.facebook} target="_blank" rel="noopener noreferrer">
-              Facebook
-            </a>
-          )}
-          {config.twitter && (
+
+          {/* {config.twitter && (
             <a href={config.twitter} target="_blank" rel="noopener noreferrer">
               Twitter
             </a>
-          )}
+          )} */}
 
-          <a href="https://ghost.org" target="_blank" rel="noopener noreferrer">
-            Ghost
-          </a>
-
-          <Link to="/rss.xml">RSS</Link>
         </SiteFooterNav>
       </div>
     </footer>
